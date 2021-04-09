92d1106fe6cdef8016c4fea842bed2711ad2d2c6
Roshan Poudel <roshan@Roshans-Macbook.local>
2021-04-09 16:56:03 +0900

    added array initializer with css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Array from "./Array";
+import Nav from "./Nav";
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Adds n random numbers to the array
+const initializeArray = () => {
+	let array = [];
+	for (let i = 0; i < 20; i++) {
+		array.push(Math.floor(Math.random() * 100));
+	}
+	return array;
+};
+
+const App = () => {
+	let array = initializeArray();
+	const [arrayState, setArrayState] = useState(array);
+
+	return (
+		<div className="App">
+			<Nav arrayState={arrayState} setArrayState={setArrayState} />
+			<Array arrayState={arrayState} setArrayState={setArrayState} />
+		</div>
+	);
+};
 
 export default App;
